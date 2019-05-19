2c8dd6eaeabace624cd08fe6a8d95566242d5f63
Matteo Pasotti <matteopasotti1@gmail.com>
2019-05-19 11:40:11 +0100

    created mainactivity and layout

---
+++ app/src/main/java/com/example/timerlifecycle/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.timerlifecycle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.timerlifecycle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+
+}
